f06b066ab802d8b8c6050260293c5853772c7c52
Oleksandr <mrrgoodcat@gmail.com>
2017-02-02 19:54:18 +0200

    dfghidf

--- ImgurDesktop/MainWindow.xaml.cs
+++ ImgurDesktop/MainWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace ImgurDesktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Poster> Films;
         public MainWindow()
         {
             InitializeComponent();
+            Films = new List<Poster>();
+            addFilms();
+            Loaded += MainWindow_Loaded;
+        }
+
+        void addFilms()
+        {
+            Poster batman = new Poster("Batman", @"D:\Git Projects\ImgurDesktop\ImgurDesktop\ImgurDesktop\Files\mpeoHIiTODJXiCWtDuSHTnw.jpg");
+            Films.Add(batman);
+
+            Poster batman1 = new Poster("Batman1", @"D:\Git Projects\ImgurDesktop\ImgurDesktop\ImgurDesktop\Files\mpeoHIiTODJXiCWtDuSHTnw.jpg");
+            Films.Add(batman1);
+
+            Poster batman2 = new Poster("Batman2", @"D:\Git Projects\ImgurDesktop\ImgurDesktop\ImgurDesktop\Files\mpeoHIiTODJXiCWtDuSHTnw.jpg");
+            Films.Add(batman2);
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentListBox.ItemsSource = Films;
         }
     }
 }
---
+++ ImgurDesktop/Poster.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ImgurDesktop
+{
+    public class Poster
+    {
+        string name;
+        string img;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Img
+        {
+            get
+            {
+                return img;
+            }
+
+            set
+            {
+                img = value;
+            }
+        }
+
+        public Poster(string FilmName, string FilmPosterPath)
+        {
+            Name = FilmName;
+            Img = FilmPosterPath;
+        }
+    }
+}
